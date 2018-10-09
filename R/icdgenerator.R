40b538d8f8b5596bb9c7177adc133592654b7caa
Peter DeWitt <peter.dewitt@ucdenver.edu>
2018-10-09 17:16:33 -0600

    works, but can't generate the big sets

--- R/icdgenerator.R
+++ R/icdgenerator.R
@@ -9,8 +9,8 @@
 #' @param gcols  number of "other" columns to generate
 #' @param pct_empty the percentage of diagnostic and procedure codes to be
 #' "missing" in the resulting data frame.
-#' @param quiet If \code{FALSE}, the default, then report incrimental timing.
-#' To suprress timing set \code{quiet = TRUE}.
+#' @param quiet If \code{FALSE}, then report incremental timing.
+#' To suppress incremental timing set \code{quiet = TRUE}.
 #'
 #' @examples
 #'
@@ -24,7 +24,7 @@
 #'
 #' head(eg)
 #' @export
-generate_sample <- function(version = 10, nrows = 1, dcols = 1, pcols = 1, gcols = 0, pct_empty = 0.20, quiet = FALSE) {
+generate_sample <- function(version = 10, nrows = 1, dcols = 1, pcols = 1, gcols = 0, pct_empty = 0.20, quiet = TRUE) {
 
   if (!(version %in% c(9, 10))) stop("version must be 9 or 10")
   nrows <- as.integer(nrows)
@@ -43,39 +43,47 @@ generate_sample <- function(version = 10, nrows = 1, dcols = 1, pcols = 1, gcols
   dxri <- seq(1, nrow(e[[paste0("icd", version, "dx")]]))
   pcri <- seq(1, nrow(e[[paste0("icd", version, "pc")]]))
 
-  # dx <- pc <- g <- matrix(nrow = nrows, ncol = 0)
+  dx <- pc <- dn <- pn <- gn <- character()
+  g <- numeric()
+  d_i <- d_j <- p_i <- p_j <- g_i <- g_j <- integer()
 
   tictoc::tic("Generating Diagnostic Codes")
   if (dcols > 0L) {
     dx <- sample(dxri, size = nrows * dcols * (1 - pct_empty), replace = TRUE)
-    d_i = sample(seq(1, nrows, by = 1), length(dx), replace = TRUE)
-    d_j = sample(seq(1, dcols, by = 1), length(dx), replace = TRUE)
+    dx <- e[[paste0("icd", version, "dx")]]$code[dx]
+    d_i <- sample(seq(1, nrows, by = 1), length(dx), replace = TRUE)
+    d_j <- sample(seq(1, dcols, by = 1), length(dx), replace = TRUE)
+    dn  <- paste("d", 1:dcols, sep = "_")
   }
   tictoc::toc(quiet = quiet)
 
   tictoc::tic("Generating Procedure Codes")
   if (pcols > 0L) {
     pc <- sample(pcri, size = nrows * pcols * (1 - pct_empty), replace = TRUE)
+    pc <- e[[paste0("icd", version, "pc")]]$code[pc]
     p_i <- sample(seq(1, nrows, by = 1), length(pc), replace = TRUE)
     p_j <- sample(seq(1, pcols, by = 1), length(pc), replace = TRUE)
+    pn  <- paste("p", 1:pcols, sep = "_")
   }
   tictoc::toc(quiet = quiet)
 
-  tictoc::tic("Generating gcols or rnorm data")
+  tictoc::tic("Generating gcols of random data")
   if (gcols > 0L) {
-    g  <- stats::rnorm(n = nrows * gcols * (1 - pct_empty))
+    g  <- sample(c(letters, LETTERS), size = nrows * gcols * (1 - pct_empty), replace = TRUE)
     g_i <- sample(seq(1, nrows, by = 1), length(g), replace = TRUE)
     g_j <- sample(seq(1, gcols, by = 1), length(g), replace = TRUE)
+    gn  <- paste("g", 1:gcols, sep = "_")
   }
   tictoc::toc(quiet = quiet)
 
-  tictoc::tic("Building the data set as a sparce matrix")
-  # out <- cbind(dx, pc, g)
-  out <- Matrix::spMatrix(nrow = nrows,
-                          ncol = dcols + pcols + gcols,
-                          i    = c(d_i, p_i, g_i),
-                          j    = c(d_j, p_j, g_j),
-                          x    = c(dx, pc, g))
+  tictoc::tic("Building the data set as a sparse matrix")
+  out <- matrix(character(), nrow = nrows, ncol = dcols + pcols + gcols)
+  rws <- c(d_i, p_i, g_i)
+  cls <- c(d_j, dcols + p_j, dcols + pcols + g_j)
+  dat <- c(dx, pc, g)
+  for(i in seq(1, length(dx) + length(pc) + length(g))) {
+    out[rws[i], cls[i]] <- dat[i]
+  }
   tictoc::toc(quiet = quiet)
 
   tictoc::tic("coercing to standard matrix")
@@ -86,7 +94,8 @@ generate_sample <- function(version = 10, nrows = 1, dcols = 1, pcols = 1, gcols
   out <- dplyr::as_data_frame(out)
   tictoc::toc(quiet = quiet)
 
-  tictoc::tic("adding id column")
+  tictoc::tic("adding id column and naming generated data columns")
+  names(out) <- c(dn, pn, gn) 
   out <- tibble::add_column(out, id = seq(1, nrows, by = 1), .before = 1L)
   tictoc::toc(quiet = quiet)
 
